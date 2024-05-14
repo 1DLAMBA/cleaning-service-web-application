cee868de7924588ec356a5df748d8ce2bcab9d17
Daniel Alamba <64867861+1DLAMBA@users.noreply.github.com>
2024-05-14 11:14:18 +0100

    Update environment.ts
    environment changes

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment={
-    apiUrl: 'http://127.0.0.1:8000/api'
+    apiUrl: 'https://aandbcleaner.org/cleaning-service-API/public/api'
     // apiUrl: 'http://192.168.55.149:8000/api'
     // 
 }
