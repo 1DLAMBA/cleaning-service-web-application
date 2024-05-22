809ddb1e4d9f13c35bcfbbf84af76b0f146dc27d
Daniel <danielalamba15@gmail.com>
2024-05-22 17:17:02 +0100

    social media integration

--- src/app/navs/navigation/navigation.component.ts
+++ src/app/navs/navigation/navigation.component.ts
@@ -12,4 +12,16 @@ export class NavigationComponent {
   openFullscreen(content: TemplateRef<any>) {
 		this.modalService.open(content, { fullscreen: true });
 	}
+
+  instagram(){
+    window.open('https://www.instagram.com/ABCLEANERJANITORIALSERVICE', '_blank');
+  }
+  facebook(){
+    window.open('https://www.facebook.com/share/mv1SzkZdqjSM8i3d/?mibextid=qi2Omg', '_blank');
+
+  }
+  email(){
+    window.location.href=`mailto:aandcleaner@gmail.com`
+  }
+
 }
